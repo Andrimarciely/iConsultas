0c38617d9337ef755e4303ecf9d7d5961aa44a26
Andrimarciely <2019003790@ifam.edu.br>
2022-05-04 13:56:24 -0300

    Initial Commit

---
+++ iConsultas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iConsultas
+//
+//  Created by Aluno01 on 03/05/22.
+//  Copyright © 2022 Eldorado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
